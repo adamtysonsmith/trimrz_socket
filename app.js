7ece1f815de15ff1c4f988303b571dbb557c0bdd
Adam <atsmith227@gmail.com>
2017-05-28 18:47:10 -0600

    changed default port for local dev

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const io = require('socket.io')();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 
 io.listen(port);
+console.log(`INFO: Socket server listening on port ${port}`);
 
-io.on('connection', socket => console.log('INFO: Socket server connected to client.', socket));
+io.on('connection', socket => console.log('INFO: Socket server connected to client'));
 
-io.on('error', err => console.log('ERROR: Socket server error event.', err));
+io.on('error', err => console.log('ERROR: Socket server error event', err));
