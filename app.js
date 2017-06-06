0ab30563590f535266182d8146334db41155d34d
Adam <atsmith227@gmail.com>
2017-06-05 21:36:15 -0600

    broadcasting events to all clients

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const io = require('socket.io')();
+const express = require('express');
 const port = process.env.PORT || 4000;
-
-io.listen(port);
-console.log(`INFO: Socket server listening on port ${port}`);
-
-io.on('connection', socket => console.log('INFO: Socket server connected to client'));
+const server = express().listen(port);
+const io = require('socket.io')(server);
+console.log('INFO: Listening on port:', port);
 
 io.on('error', err => console.log('ERROR: Socket server error event', err));
+
+io.on('connection', client => {
+  console.log('INFO: Connected to client', client.id);
+
+  const hired = 'hired event';
+  const applied = 'job response event';
+  const job = 'new job event';
+
+  client.on(hired, broadcastToClients(hired))
+  client.on(applied, broadcastToClients(applied))
+  client.on(job, broadcastToClients(job))
+
+  client.on('disconnect', () => console.log('INFO: Client disconnected', client.id))
+});
+
+
+function broadcastToClients(eventName) {
+  return (data) => io.emit(eventName, data);
+}
